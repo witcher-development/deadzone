3b0667a541f0d9c4c436f5eb3bebe918271f0115
roman <roman@luchs-sec.com>
2024-09-26 00:29:47 +0200

    serve static

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"deadzone/db"
 	"deadzone/zone"
+	"deadzone/ui"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,6 +12,11 @@ import (
 func main() {
 	router := http.NewServeMux()
 	router.Handle("/zone", zone.GetRouter())
+
+	router.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, ui.Index)
+	})
+	router.Handle("GET /static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static"))))
 
 	server := http.Server{
 		Addr:    ":3000",
---
+++ ui/routes.go
@@ -0,0 +1,13 @@
+package ui
+
+var Index = `
+	<html>
+	<head>
+		<title>DeadZone Map</title>
+		<link rel="stylesheet" href="/static/styles.css" />
+	</head>
+	<body>
+		<h1>hi</h1>
+	</body>
+	</html>
+`
