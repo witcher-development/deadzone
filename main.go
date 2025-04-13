14736cca2b6975ed5dab7d98e7e8651176c821ca
roman <roman@luchs-sec.com>
2025-04-13 02:29:03 +0200

    Revert "playing with CacheControl API"
    This reverts commit c64b39bd4b8d8e7b662d24c15b7738f1d45d9b68.

--- main.go
+++ main.go
@@ -16,8 +16,6 @@ func main() {
   static := r.Group("/")
   static.Use(func(c *gin.Context) {
     c.Writer.Header().Set("Cache-Control", "public, max-age=0, immutable")
-    // TODO: only apply to SW
-    c.Writer.Header().Set("Service-Worker-Allowed", "/")
   })
   static.Static("/static", "./static")
 
